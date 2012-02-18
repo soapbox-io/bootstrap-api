6fb495f25489919fe35b85483dff58698e023537
Danny Kopping <dannykopping@gmail.com>
2012-02-18 14:25:49 +0200

    Added exception handling

--- parser.php
+++ parser.php
@@ -3,13 +3,71 @@
     spl_autoload_register("autoload");
 
     // load HTML template
-    $content = file_get_contents(dirname(__FILE__) . "/template.html");
+    $content = @file_get_contents(dirname(__FILE__) . "/template.html");
+
+    if(empty($content))
+        die("The <strong>template.html</strong> file could not found or is not readable in ".dirname(__FILE__));
 
     // load configuration file (with CDATA sections being converted to strings)
+    libxml_use_internal_errors(true);
     $config = simplexml_load_file(dirname(__FILE__) . "/config.xml", "SimpleXMLElement", LIBXML_NOCDATA);
 
+    $ignoredSections = array("errorElements");
+
+    $errors = libxml_get_errors();
+
+    if(!empty($errors))
+    {
+        $elements = array();
+
+        $ignoredSections = array("sectionElements", "sectionListElements");
+
+        $titleElement = new TitleElement();
+        $titleElement->setTitle("Error parsing configuration file");
+        $titleElement->setSubTitle("Please review the messages below");
+
+        $elements[] = $titleElement;
+
+        foreach($errors as $error)
+        {
+//            echo "<pre>".trim($error->message)." on line ".$error->line."</pre>\n";
+
+            $errorElement = new AlertBoxElement();
+            $errorElement->setText(ucfirst(trim($error->message))." on <strong>line ".$error->line."</strong>");
+
+            $severity = $error->level;
+            switch($severity)
+            {
+                default:
+                case LIBXML_ERR_WARNING:
+                    $errorElement->setTitle("Notice");
+                    $errorElement->setType(AlertBoxElement::INFO_TYPE);
+                    break;
+                case LIBXML_ERR_ERROR:
+                    $errorElement->setTitle("Warning");
+                    $errorElement->setType(AlertBoxElement::WARNING_TYPE);
+                    break;
+                case LIBXML_ERR_FATAL:
+                    $errorElement->setTitle("Fatal Error");
+                    $errorElement->setType(AlertBoxElement::ERROR_TYPE);
+                    break;
+            }
+
+            $elements[] = $errorElement;
+        }
+
+        $content = replaceTokens($elements, $content, true, $ignoredSections);
+
+        // final output to file - html in this case
+        die($content);
+    }
+    else
+    {
+
+    }
+
     if(empty($config))
-        die("config.xml file could not found or is not readable in ".dirname(__FILE__));
+        die("<strong>config.xml</strong> file could not found or is not readable in ".dirname(__FILE__));
 
     $elements = array();
 
@@ -34,12 +92,12 @@
         }
     }
 
-    $content = replaceTokens($elements, $content);
+    $content = replaceTokens($elements, $content, false, $ignoredSections);
 
     // final output to file - html in this case
     echo $content;
 
-    function replaceTokens($elements, $content)
+    function replaceTokens($elements, $content, $alertsAsExceptions=false, $ignoredSections=null)
     {
         // find common element names, and group them
         $groups = array();
@@ -49,10 +107,21 @@
             if(empty($element))
                 continue;
 
-            if(!isset($groups[$element->getElementName()]))
-                $groups[$element->getElementName()] = array();
+            $elementName = $element->getElementName();
+            if($elementName == "alertBoxElement" && $alertsAsExceptions)
+                $elementName = "errorElements";
 
-            $groups[$element->getElementName()][] = $element;
+            if(!isset($groups[$elementName]))
+                $groups[$elementName] = array();
+
+            $groups[$elementName][] = $element;
+        }
+
+        // ignore sections by replacing their placeholders with blank data
+        if(!empty($ignoredSections))
+        {
+            foreach($ignoredSections as $ignored)
+                $content = str_replace("{{" . $ignored . "}}", "", $content);
         }
 
         if(empty($groups))
@@ -61,6 +130,7 @@
         foreach($groups as $type => $elements)
         {
             $combinedElements = "";
+
             if(empty($type) || empty($elements))
                 continue;
 
